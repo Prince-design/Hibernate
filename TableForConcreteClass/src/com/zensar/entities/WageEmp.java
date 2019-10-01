589e23e7e71d675b88a75fe8b3a750ee6124bfa7
Prince-design <princetiwari59@gmail.com>
2019-10-01 12:34:33 -0700

    Initial Commit Added TableForConcreteclass

---
+++ TableForConcreteClass/src/com/zensar/entities/WageEmp.java
@@ -0,0 +1,47 @@
+package com.zensar.entities;
+/**
+ *@author Prince Tiwari
+ *@creation_date 27th Sep 2019 11.53am
+ *@Modification_date 27th Sep 2019 11.53am
+ *@version 1.0
+ *@copyright Zensar Technologies. All rights reserved.
+ *@description Persistence class
+ */
+
+import java.time.LocalDate;
+
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="WageEmp_1")
+public class WageEmp extends Employee {
+
+public WageEmp(int empId, String name, LocalDate joinDate, double salary, int hours, float rate) {
+		super(empId, name, joinDate, salary);
+		this.hours = hours;
+		this.rate = rate;
+	}
+private int hours;
+private float rate;
+public WageEmp() {
+	// TODO Auto-generated constructor stub
+}
+public int getHours() {
+	return hours;
+}
+public void setHours(int hours) {
+	this.hours = hours;
+}
+public float getRate() {
+	return rate;
+}
+public void setRate(float rate) {
+	this.rate = rate;
+}
+@Override
+public String toString() {
+	return "WageEmp [hours=" + hours + ", rate=" + rate + "]";
+}
+}
